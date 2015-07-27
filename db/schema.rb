6c7d3e231e41e18587ad75d74df8938bd500f479
PabiGamito <pablogamito@gmail.com>
2015-07-26 22:28:19 -0400

    End of day update. Got navbar style working.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726144546) do
+ActiveRecord::Schema.define(version: 20150726144601) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 20150726144546) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id"
+    t.string   "post"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
